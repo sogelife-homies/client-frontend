4df3f421305d2528bbd3b40ca3e290a1be0e16fc
Egor Ermolaev <egor.ermolaev@uni.lu>
2024-03-02 23:27:26 +0100

    add(db): mocked storage of kpiId->kpiProps, and LLM's input

---
+++ app/src/Services/Database.js
@@ -0,0 +1,18 @@
+const database = {
+    "1": "Carbon Emission, less than, million metric tons per year",//400
+    "2": "Energy Consumption, less than, giga joules per year",//120,000
+    "3": "Water Withdrawal, less than, million cubic meters per year",//5
+    "4": "Employee Training Hours, more than, hours per employee per year",//50
+    "5": "Community Investments, more than, millions euro per year",//50
+    "6": "Waste Generated, less than, metric tons per year",//50,000
+}
+
+//ChatGPT will expect the following input
+const kpis = {
+    "Carbon Emission, less than, million metric tons per year": 400,
+    "Energy Consumption, less than, giga joules per year": 120000,
+    "Water Withdrawal, less than, million cubic meters per year": 5,
+    "Employee Training Hours, more than, hours per employee per year": 50,
+    "Community Investments, more than, millions euro per year": 50,
+    "Waste Generated, less than, metric tons per year": 50000,
+}
